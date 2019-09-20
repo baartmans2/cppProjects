c310188e9c986e531ab1d984751199f68adfdc02
U-BSD\353868 <353868@SHS-10DECC936.beaverton.k12.or.us>
2019-09-20 08:11:28 -0700

    palindrome done & commented

--- palindrome.cpp
+++ palindrome.cpp
@@ -16,31 +16,40 @@ int main()
   char array3 [80];//stores reversed chars
   int spot = 0;//the location of the terminating nullchar
   cout << "Enter in a bunch of characters - up to 80." << endl;
-  cin >> input;
+  cin.getline(input, 80);
   if (sizeof(input) == 0)//im lazy so i didnt bother deleting this since it will never execute
     {
       cout << "Enter something!";
     }
   else
     {
+      for (int i = 0; i < 80; i++)//convert all to lower case
+	{
+	  if (input[i] < 91 && input[i] > 64)
+	    {
+	      input[i] = input[i] + 32;
+	    }
+	}
+      
       int counter = 0; //used in compressing input
       for (int i = 0; i < 80; i++)
 	{//read through the array and insert all non puncuation stuff into array2
-	  if ((( input[i] >= 65) && (input[i] <= 90)) || ((input[i] >= 97) && (input[i] <= 122)))//char values for normal characters
+	  if ( ( ( input[i] >= 97) && (input[i] <= 122) ) )//char values for normal characters
 	    {
 	      array2[counter] = input[i];
 	      counter++;
 	    }
 	  else if (input[i] == '\0')//if we see the terminating null char, save that spot and quit the loop
 	    {
-	      spot = i;
+	      cout << input[i] << endl;
+	      spot = i - 1;
 	      i = 80;
 	    }
 	}
       counter = 0;
       for (int i = spot; i >= 0; i--)//move backwards from the spot and reverse the input
 	{
-	  if ( ((array2[i] >= 65) &&  (array2[i] <= 90)) || ((array2[i] >= 97) && (array2[i] <= 122)) )
+	  if ( ((array2[i] >= 97) &&  (array2[i] <= 122)))
 	    {
 	      array3[counter] = array2[i];
 	      counter++;
@@ -48,7 +57,7 @@ int main()
 	}
       
       
-      for (int i = 0; i <= spot; i++)
+      for (int i = 0; i <= spot; i++)//print out words so the person can see
 	{
 	  cout << array2[i] << endl;
 	}
